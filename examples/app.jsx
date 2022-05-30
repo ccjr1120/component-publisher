10a9f26db88a0934128dc4fe8bf2506ab1fb2928
ccjr <qianaiwuyu@gmail.com>
2022-05-30 15:03:58 +0800

    add examples

---
+++ examples/app.jsx
@@ -0,0 +1,9 @@
+import ReactComponent from './components/ReactComponent';
+import ReactTsComponent from './components/ReactTsComponent';
+
+const App = () => <ReactTsComponent />;
+const App = () => <ReactComponent />;
+
+const rootNode = document.getElementById('root');
+
+render(<App />, rootNode);
